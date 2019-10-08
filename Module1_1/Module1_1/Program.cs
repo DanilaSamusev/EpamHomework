75755bdabfe023793b0adb873577d326bad84663
Aldamar <danila.samusev@mail.ru>
2019-10-08 15:08:11 +0300

    Task complited.

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            
+            Console.WriteLine("a: " + a + " b: " + b);
+            Swap(ref a, ref b);
+            Console.WriteLine("a: " + a + " b: " + b);
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
