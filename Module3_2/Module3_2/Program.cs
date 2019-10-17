35153e8fa2d3a2e55d4a808bcf6f6e00a168fa78
Aldamar <danila.samusev@mail.ru>
2019-10-17 19:19:14 +0300

    Task complete.

---
+++ Module3_2/Module3_2/Program.cs
@@ -0,0 +1,39 @@
+﻿﻿using System;
+
+namespace Module3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetNumber("number: ");
+            PrintNumbers(number);
+        }
+        
+        static int GetNumber(string query)
+        {
+            while (true)
+            {
+                Console.Write($"Inter the {query}");
+                bool isNumberParsed = int.TryParse(Console.ReadLine(), out int number);
+
+                if (isNumberParsed)
+                {
+                    return number;
+                }
+                
+                Console.WriteLine("Error: input data has to be an integer\n-------------------------");
+            }
+        }
+
+        static void PrintNumbers(int numbersCount)
+        {
+            Console.Write("result: ");
+            
+            for (var i = 1; i <= numbersCount; i++)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
