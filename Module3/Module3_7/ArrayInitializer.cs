58949333fb13e2243d7a30bfe150e6e44d23af71
Aldamar <danila.samusev@mail.ru>
2019-10-22 20:57:01 +0300

    Task 7 complete.

---
+++ Module3/Module3_7/ArrayInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Module3_7
+{
+    public class ArrayInitializer
+    {
+        public double[] InitializeArray()
+        {
+            int arraySize = GetArraySize();
+            double[] array = new double[arraySize];
+
+            for (int i = 0; i < arraySize;)
+            {
+                Console.Write($"Inter the {i + 1} array number: ");
+
+                if (double.TryParse(Console.ReadLine(), out double number))
+                {
+                    array[i] = number;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Error: array element has to be a number!\n--------------------------------");
+                }
+            }
+
+            return array;
+        }
+
+        private int GetArraySize()
+        {
+            while (true)
+            {
+                Console.Write("Inter the array size: ");
+
+                if (int.TryParse(Console.ReadLine(), out int arraySize) && arraySize > 0)
+                {
+                    return arraySize;
+                }
+                
+                Console.WriteLine("Error: array size has to be a positive integer!\n--------------------------------");
+            }
+        }
+    }
+}
