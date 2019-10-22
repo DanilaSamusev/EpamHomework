73344873b529d294501b254d0d459e396963fe13
Aldamar <danila.samusev@mail.ru>
2019-10-22 14:39:59 +0300

    Module_2 Task_1 has been refactored.

--- Module2_1/Module2_1/Program.cs
+++ Module2_1/Module2_1/Program.cs
@@ -4,15 +4,15 @@ namespace Module2_1
 {
     class Program
     {
-        private const string CompaniesQuery = "number of companies: ";
-        private const string TaxQuery = "tax per company: ";
+        private const string NumberOfCompanies = "number of companies: ";
+        private const string TaxRate = "tax per company: ";
         private const string CompanyProfit = "company profit: ";
 
         static void Main(string[] args)
         {
-            double companyProfit = GetDoubleData(CompanyProfit);
-            int companiesCount = GetIntegerData(CompaniesQuery);
-            float taxPerCompany = GetDoubleData(TaxQuery);
+            double companyProfit = GetCompanyProfit(CompanyProfit);
+            int companiesCount = GetIntegerData(NumberOfCompanies);
+            float taxPerCompany = GetTaxRate(TaxRate);
             double totalCompaniesTax = CountTotalCompaniesTax(companiesCount, taxPerCompany, companyProfit);
             Console.WriteLine($"Total tax: {totalCompaniesTax}");
         }
@@ -21,7 +21,7 @@ namespace Module2_1
         {
             while (true)
             {
-                Console.Write($"Inter the {query}");
+                Console.Write($"Enter the {query}");
                 bool isDataParsed = int.TryParse(Console.ReadLine(), out int companiesCount);
 
                 if (isDataParsed && companiesCount >= 0)
@@ -33,11 +33,27 @@ namespace Module2_1
             }
         }
 
-        static float GetDoubleData(string query)
+        static float GetCompanyProfit(string query)
         {
             while (true)
             {
-                Console.Write($"Inter the {query}");
+                Console.Write($"Enter the {query}");
+                bool isDataParsed = float.TryParse(Console.ReadLine(), out float data);
+
+                if (isDataParsed && data >= 0 && data <= 100)
+                {
+                    return data;
+                }
+
+                Console.WriteLine("Error: input data has to be a positive number!\n-----------------------------");
+            }
+        }
+        
+        static float GetTaxRate(string query)
+        {
+            while (true)
+            {
+                Console.Write($"Enter the {query}");
                 bool isDataParsed = float.TryParse(Console.ReadLine(), out float data);
 
                 if (isDataParsed && data >= 0 && data <= 100)
